cf5f566aec697e476e12c8de0e51a28b74019f1f
Harshith Mullapudi <harshithmullapudi@gmail.com>
2025-07-28 16:03:52 +0530

    Feat: added privacy, terms

---
+++ src/pages/terms.tsx
@@ -0,0 +1,19 @@
+import { ScrollArea } from '@redplanethq/ui';
+
+import { Container, Header } from 'src/components';
+
+const Terms = () => {
+  return (
+    <div className="flex min-h-svh h-[100vh] flex-col items-center justify-start overflow-hidden bg-background">
+      <ScrollArea className="overflow-auto flex flex-col h-full w-full">
+        <Header />
+
+        <Container className="pt-[3rem] max-w-[40rem]">
+          <h2 className="text-2xl"> Terms</h2>
+        </Container>
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default Terms;
